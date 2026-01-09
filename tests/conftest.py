da059d4015671b00e8c018f00d965318fabf4dbf
Lennon Couto <lennon.couto@gmail.com>
2026-01-08 23:57:13 -0300

    Adicionei na table User - Updated_at

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,10 +27,12 @@ def session():
 
 
 @contextmanager
-def _mock_db_time(model, time=datetime(2026, 1, 7)):
+def _mock_db_time(*, model, time=datetime(2026, 1, 7)):
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
 
     event.listen(model, 'before_insert', fake_time_hook)
 
